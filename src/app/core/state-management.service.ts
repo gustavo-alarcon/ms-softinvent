d1a998e3904cb8d07ac083c8d54d694046e0b151
Gustavo Alarcón <galarcon@meraki-s.com>
2019-05-21 00:52:29 -0500

    stateManagement observable

--- src/app/core/state-management.service.ts
+++ src/app/core/state-management.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Ticket } from "./ms-types";
 import { CurrencyIndex } from '@angular/common/src/i18n/locale_data';
+import { MsTicket } from "../core/classes/ticket";
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateManagementService {
-  
+
   currentStateIndex: number;
-  ticketsStateManagement: Array<Ticket> = [
+
+  currentState: Array<Ticket> = [
     {
       cart: [
         {
@@ -16,7 +19,7 @@ export class StateManagementService {
           stock: 4,
           quantity: 1,
           discountType: 'discount',
-          discount: {amount: 100, percentage: 10},
+          discount: { amount: 100, percentage: 10 },
           salePrice: 1200.00,
           warehouse: "TDA 410"
         },
@@ -25,7 +28,7 @@ export class StateManagementService {
           stock: 10,
           quantity: 1,
           discountType: 'discount',
-          discount: {amount: 100, percentage: 10},
+          discount: { amount: 100, percentage: 10 },
           salePrice: 400.00,
           warehouse: "Fortaleza"
         }
@@ -38,7 +41,7 @@ export class StateManagementService {
           stock: 2,
           quantity: 2,
           discountType: 'discount',
-          discount: {amount: 100, percentage: 10},
+          discount: { amount: 100, percentage: 10 },
           salePrice: 1300.00,
           warehouse: "TDA 410"
         },
@@ -47,23 +50,35 @@ export class StateManagementService {
           stock: 10,
           quantity: 10,
           discountType: 'discount',
-          discount: {amount: 100, percentage: 10},
+          discount: { amount: 100, percentage: 10 },
           salePrice: 600.00,
           warehouse: "Fortaleza"
         },
       ], customer: 'Gabriela, Nuñez'
     }
   ];
-  i : number;
+
+  public stateManagement = new BehaviorSubject(this.currentState);
+  public ticketsStateManagement = this.stateManagement.asObservable();
+
   constructor() { }
-  agregarProducto(producto){
+
+  agregarProducto(producto) {
     this.ticketsStateManagement[this.currentStateIndex].cart.push(producto);
+    console.log(this.currentState);
+    this.agregarTicket('asd');
   }
   eliminarProducto(producto){
     this.ticketsStateManagement[this.currentStateIndex].cart.splice(this.ticketsStateManagement[this.currentStateIndex].cart.indexOf(producto),1);
   }
   agregarTicket(ticket){
     this.ticketsStateManagement.push(ticket);
+
+  public agregarTicket(asd) {
+    let newTicket: Ticket;
+    this.currentState.push(newTicket);
+    this.stateManagement.next(this.currentState);
+    console.log(this.currentState);
   }
   eliminarTicket(index){
     this.ticketsStateManagement.splice ( index,1)
